69d95ada401dd901a03b70f439f2d092423c55eb
ammar <ammarraneez@gmail.com>
2022-09-19 19:22:59 +0530

    Add getStaticProps for data population

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -43,3 +43,76 @@ export const getStaticPaths: GetStaticPaths = async () => {
     fallback: 'blocking',
   };
 };
+
+// Static Site Generation - fetch static data on build time & cache
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const querySingle = `
+    *[_type=='post' && slug.current == $slug][0] {
+      _id,
+      _createdAt,
+      title,
+      author -> {
+        name,
+        bio,
+        last,
+        image,
+        'imageUrl': image.asset->url
+      },
+      'comments': *[
+        _type == 'comment' && 
+        post._ref == ^._id  && approved == true
+      ] {
+        name,
+        last,
+        comment
+        ,
+        _createdAt
+        ,
+        userImage
+      },
+      description,
+      mainImage,
+      title,
+      slug,
+      body
+    }
+  `;
+
+  const queryAll = `
+    *[_type=='post'] {
+      _id,
+      title,
+      _createdAt,
+        author -> {
+        name,
+        image
+      },
+      description,
+      mainImage,
+      body,
+      slug
+    }
+  `;
+
+  const posts = await sanityClient.fetch(queryAll);
+  const post = await sanityClient.fetch(querySingle, {
+    slug: params?.slug,
+  });
+
+  if (!post) {
+    // If no post is found, return 404 page
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      post,
+      posts,
+    },
+
+    // Incremental Static Regeneration - refetch/update after 60s & cache
+    revalidate: 60,
+  };
+};
